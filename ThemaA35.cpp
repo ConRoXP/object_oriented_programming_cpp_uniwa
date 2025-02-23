69699b16e89b3fae3dd2770c317ce5cf100bc173
Rob. P <robert-plvn@hotmail.com>
2025-02-23 22:42:37 +0200

    Add files via upload

---
+++ ThemaA35.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class UString{
+    private:
+    int N, K; //N: row, K: column
+    char** arr;
+
+    public:
+    UString();
+    UString(const int, const int);
+    ~UString();
+    char** getPtr() const;
+    int getN() const;
+    int getK() const;
+};
+
+UString::UString(){}
+
+//2D array allocation
+UString::UString(const int n, const int k){
+    N= n;
+    K= k;
+    arr= new char*[N];
+    for(int i=0; i< N; i++){
+        arr[i]= new char[K];
+    }
+}
+
+UString::~UString(){
+    for(int i=0; i< N; i++){
+        delete arr[i];
+    }
+    delete arr;
+}
+
+char** UString::getPtr() const{
+    return arr;
+}
+
+//return row
+int UString::getN() const{
+    return N;
+}
+
+//return column
+int UString::getK() const{
+    return K;
+}
+
+//output operator overload
+ostream &operator<<(ostream &os, const UString& obj){
+    int n= obj.getN();
+    int k= obj.getK();
+
+    for(int r=0; r< n; r++){
+        for(int c=0; c< k; c++){
+            //get.Ptr()[i][j] returns a pointer to array[i][j]
+            os<< (obj.getPtr()[r][c]);
+        }
+        cout<< endl;
+    }
+    return os;
+}
+
+UString create(const int n, const int k){
+        UString obj(n, k);
+        //In each [n][k] position we input a random char
+        for(int row= 0; row< n; row++){
+            for(int column=0; column< k; column++){
+                (obj.getPtr()[row][column])= ('A'+ rand()%26);
+            }
+        }
+    return obj;
+}
+
+int main(){
+    //3 rows, 8 columns
+    UString ptr= create(3, 8);
+    cout<< ptr;
+
+    return 0;
+}
